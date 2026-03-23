9507469fd8058b7046c3a89bf06bd32d8939c8e7
basimallawala <basimallawala@gmail.com>
2026-03-23 17:52:08 -0400

    Second commit for the project

---
+++ BookstoreApp/src/finalProject/BookstoreSystem.java
@@ -0,0 +1,22 @@
+package finalProject;
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Basim
+ */
+public class BookstoreSystem {
+    ArrayList<Book> bookList;
+
+    public BookstoreSystem() {
+        this.bookList = new ArrayList<>();
+    }
+    
+}
